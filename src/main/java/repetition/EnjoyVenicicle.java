e3a3724024fd5c324fe53f32875d19f07608aae4
Epap <peia@inbox.ru>
2023-10-19 11:15:19 +0300

    Inner class

---
+++ src/main/java/repetition/EnjoyVenicicle.java
@@ -0,0 +1,40 @@
+package repetition;
+
+class Vehicle {
+
+    private String name;
+
+    public Vehicle(String name) {
+        this.name = name;
+    }
+
+    class Engine {
+
+        void start() {
+            System.out.println("RRRrrrrrrr....");
+        }
+
+    }
+
+    class Body {
+        String color;
+
+        public Body(String color) {
+            this.color = color;
+        }
+
+        public void printColor() {
+            System.out.printf("Vehicle %s has %s body.", name, color);
+        }
+    }
+}
+
+class EnjoyVehicle {
+
+    public static void main(String[] args) {
+
+        Vehicle vehicle = new Vehicle("Dixi");
+        Vehicle.Body body = vehicle.new Body("red");
+        body.printColor();
+    }
+}
