56ffd120f48ac2eba249998ce936dc7d44b092f8
Epap <peia@inbox.ru>
2023-10-25 08:50:23 +0300

    Arrays.sort

---
+++ src/main/java/repetition/SortArray.java
@@ -0,0 +1,21 @@
+package repetition;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SortArray {
+
+    public static void sort(int[] numbers) {
+        Arrays.sort(numbers);
+    }
+
+    public static void main(String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+        String[] values = scanner.nextLine().split("\\s+");
+        int[] numbers = Arrays.stream(values)
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        sort(numbers);
+        Arrays.stream(numbers).forEach(e -> System.out.print(e + " "));
+    }
+}
