2d766d2bd698bfe1da0d3dce856d93b9f89cbf7f
Epap <peia@inbox.ru>
2023-11-12 18:23:25 +0300

    Dealing with modifiers

--- src/main/java/FieldGetter.java
+++ src/main/java/FieldGetter.java
@@ -1,3 +1,8 @@
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 class FieldGetter {
     /**
@@ -14,5 +19,29 @@ class FieldGetter {
 
     public int getNumberOfAccessibleFields(Class<?> clazz) {
         return clazz.getFields().length;
+    }
+
+    /**
+     * Get sorted list of private fields the object declares (inherited fields should be skipped).
+     */
+
+    public List<String> getPrivateFields(Object object) {
+        return Arrays.stream(object.getClass().getDeclaredFields())
+                .filter(a -> Modifier.isPrivate(a.getModifiers()))
+                .map(Field::getName)
+                .sorted()
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * Get list of public fields the object declares (inherited fields should be skipped).
+     */
+
+    public String[] getPublicFields(Object object) {
+        return Arrays.stream(object.getClass().getDeclaredFields())
+                .filter(a -> Modifier.isPublic(a.getModifiers()))
+                .map(Field::getName)
+                .collect(Collectors.toList())
+                .toArray(new String[]{});
     }
 }
