4838eae37662f1ee1fa3c971bd4ac6201274e773
Epap <peia@inbox.ru>
2023-11-29 09:06:51 +0300

    Repetition of threads

---
+++ src/main/java/repetition/Starter.java
@@ -0,0 +1,9 @@
+package repetition;
+
+public class Starter {
+    public static void startRunnables(Runnable[] runnables) {
+        for (Runnable runnable : runnables) {
+            new Thread(runnable).start();
+        }
+    }
+}
---
+++ src/main/java/repetition/counter.java
@@ -0,0 +1,9 @@
+package repetition;
+
+class Counter {
+    int count = 0;
+
+    public synchronized void inc() {
+        count++;
+    }
+}
