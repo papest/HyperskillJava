057d51072f05453ccd697b7265a25fd4064d36df
Epap <peia@inbox.ru>
2024-03-26 14:47:18 +0300

    removeIf

--- src/main/java/repetition/MapFunctions.java
+++ src/main/java/repetition/MapFunctions.java
@@ -9,4 +9,27 @@ class MapFunctions {
         set1.retainAll(map2.entrySet());
         System.out.println(set1.size());
     }
+
+    static final int LONG_LENGTH = 7;
+
+    public static void removeLongNames(Map<String, String> map) {
+        map.entrySet()
+                .removeIf(entry -> entry.getKey().length() > LONG_LENGTH || entry.getValue().length() > LONG_LENGTH);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Map<String, String> map = new HashMap<>();
+
+        while (scanner.hasNextLine()) {
+            String s = scanner.nextLine();
+            String[] pair = s.split(" ");
+            map.put(pair[0], pair[1]);
+        }
+
+        MapFunctions.removeLongNames(map);
+
+        System.out.println(map.size());
+    }
+
 }
