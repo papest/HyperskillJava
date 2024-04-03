ae2d05aa2d85a3731a8ca28da7e2c1ad1c74f842
Epap <peia@inbox.ru>
2024-04-03 19:00:05 +0300

    Thread state repetition

--- src/main/java/ThreadUtil.java
+++ src/main/java/ThreadUtil.java
@@ -7,4 +7,13 @@ class ThreadUtil {
             }
         }
     }
+
+    static Thread createThreadInNewState() {
+        return new Thread("This is new thread");
+    }
+
+    public static void main(String[] args) {
+        Thread thread = ThreadUtil.createThreadInNewState();
+        System.out.println(thread.getState());
+    }
 }
