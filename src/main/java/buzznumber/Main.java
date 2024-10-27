7e02a343a17ea75a029fed1faa1954ffc730690d
Epap <peia@inbox.ru>
2024-10-27 12:48:47 +0300

    duck number. LinkedHashMap

--- src/main/java/buzznumber/Main.java
+++ src/main/java/buzznumber/Main.java
@@ -1,8 +1,18 @@
 package buzznumber;
 
+import java.util.LinkedHashMap;
 import java.util.Scanner;
 
 public class Main {
+    public static void printProperty(LinkedHashMap<String, Boolean> property) {
+
+        property
+                .entrySet()
+                .stream()
+                .map(entry -> String.format("%s : %s", entry.getKey(), entry.getValue()))
+                .forEach(System.out::println);
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a natural number:");
@@ -15,10 +25,13 @@ public class Main {
         boolean end7 = number % 10 == 7;
         boolean divisible7 = number % 7 == 0;
         boolean buzz = end7 || divisible7;
-        System.out.println(even ? "This number is Even." : "This number is Odd.");
-        System.out.printf("It is %sa Buzz number.\nExplanation:\n%s ", buzz ? "" : "not ", number);
-        System.out.printf(!buzz ? "is neither divisible by 7 nor does it end with 7.\n" : divisible7 && end7 ?
-                "is divisible by 7 and ends with 7." : divisible7 ? "is divisible by 7." : " ends with 7.");
-
+        boolean duck = String.valueOf(number).contains("0");
+        System.out.printf("Property of %s\n", number);
+        printProperty(new LinkedHashMap<>(1, 5, true) {{
+            put("even", even);
+            put("odd", !even);
+            put("buzz", buzz);
+            put("duck", duck);
+        }});
     }
 }
