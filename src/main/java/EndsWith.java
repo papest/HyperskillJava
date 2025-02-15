71d1bebb4d24a34c7f9afc3794e3b367ab0e6960
Epap <peia@inbox.ru>
2025-02-15 08:39:00 +0300

    Ends with burg

---
+++ src/main/java/EndsWith.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class EndsWith {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(scanner.nextLine().endsWith("burg"));
+    }
+}
