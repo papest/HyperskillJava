3a59d28a952df4bda61514481b38ac5db58bc6bd
Epap <peia@inbox.ru>
2025-03-26 08:09:01 +0300

    Print block

---
+++ src/main/java/TicTacToePrint.java
@@ -0,0 +1,9 @@
+public class TicTacToePrint {
+    public static void main(String[] args) {
+        System.out.println("""
+                O X X
+                O X O
+                X O X"""
+        );
+    }
+}
